491830dadbc68ade3294c0227190e663c4af8c07
Rahul7744 <rahuljoshi1154@gmail.com>
2020-01-03 15:30:51 +0530

    Project

---
+++ project_4/health.js
@@ -0,0 +1,17 @@
+function cycling(){
+    var b = document.getElementById('exercise').value;
+
+    document.getElementById('exer_value').innerHTML = "You have burned " + 500 + " " + "calories";  
+}
+
+
+
+
+
+
+function apple(){
+    var a = document.getElementById('foodtype').value;
+
+    document.getElementById('food_value').innerHTML = "You have gained " + 100 + " " + "calories";
+}
+
